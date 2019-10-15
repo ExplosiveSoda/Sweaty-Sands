05583130cce66c111d0f3457ce23157412d44c63
Tyler <tyler.mattioli@mt.gov>
2019-10-15 14:25:07 -0600

    new pois

--- src/app/shared/data/poi.ts
+++ src/app/shared/data/poi.ts
@@ -2,95 +2,55 @@ import { PoiLocations } from '../interfaces/poi-locations';
 
 export const POI: PoiLocations[] = [
     {
-        name: 'Junk Junction',
-        location: [18024, 3342]
+      name: 'Craggy Cliffs',
+      location: [17241, 11968]
     },
     {
-        name: 'Starry Suburbs',
-        location: [17122, 4760]
+      name: 'Steamy Stacks',
+      location: [16080, 17136]
     },
     {
-        name: 'Haunted Hills',
-        location: [16320, 2352]
+      name: 'Pleasant Park',
+      location: [14848, 8800]
     },
     {
-        name: 'Pleasant Park',
-        location: [14422, 5106]
+      name: 'Sweaty Sands',
+      location: [12504, 4520]
     },
     {
-        name: 'Loot Lake',
-        location: [12696, 8354]
+      name: 'Salty Springs',
+      location: [10984, 8216]
     },
     {
-        name: 'The Block',
-        location: [17342, 7282]
+      name: 'Frenzy Farm',
+      location: [12264, 13360]
     },
     {
-        name: 'Lazy Lagoon',
-        location: [15324, 11704]
+      name: 'Dirty Docks',
+      location: [11168, 18496]
     },
     {
-        name: 'Sunny Steps',
-        location: [15688, 16612]
+      name: 'Holly Hedges',
+      location: [9088, 4064]
     },
     {
-        name: 'Pressure Plant',
-        location: [14152, 14996]
+      name: 'Weeping Woods',
+      location: [7775, 7224]
     },
     {
-        name: 'Lonely Lodge',
-        location: [11576, 18108]
+      name: 'Slurpy Swamp',
+      location: [5143, 6440]
     },
     {
-        name: 'Retail Row',
-        location: [9327, 14888]
+      name: 'Lazy Lake',
+      location: [6504, 14084]
     },
     {
-        name: 'Dusty Depot',
-        location: [10044, 11808]
+      name: 'Retail Row',
+      location: [7132, 17184]
     },
     {
-        name: 'Gotham City',
-        location: [10000, 6988]
-    },
-    {
-        name: 'Snobby Shores',
-        location: [11072, 980]
-    },
-    {
-        name: 'Greasy Grove',
-        location: [7467, 3992]
-    },
-    {
-        name: 'Polar Peak',
-        location: [5499, 4548]
-    },
-    {
-        name: 'Shifty Shafts',
-        location: [7147, 6960]
-    },
-    {
-        name: 'Salty Springs',
-        location: [7624, 11352]
-    },
-    {
-        name: 'Moisty Palms',
-        location: [4575, 16820]
-    },
-    {
-        name: 'Fatal Fields',
-        location: [4427, 11928]
-    },
-    {
-        name: 'Frosty Flights',
-        location: [5009, 1648]
-    },
-    {
-        name: 'Happy Hamlet',
-        location: [2404, 6116]
-    },
-    {
-        name: 'Lucky Landing',
-        location: [1232, 11224]
+      name: 'Misty Meadows',
+      location: [3412, 11988]
     }
 ];
